a6e136fdb8d413eeec60554bbdd28386fcd07450
felipe <felipesscreator@gmail.com>
2022-08-22 21:41:09 -0300

    feat: starting componenting

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+import { api } from '../services/api';
+
+import './styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Id {
+  filmId: number;
+}
+
+export function Content(props: Id) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  setSelectedGenreId(props.filmId);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+
+  return(
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Id {
+  filmId: number;
+}
+
+export function Header(props: Id) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  setSelectedGenreId(props.filmId);
+
+  useEffect(() => {
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return(
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+  )
+}
